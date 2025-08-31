87590e60d511df81a139c0ce51a0aaa01b1b1c4e
MoNRaSpGit <ju4nrsuarez@gmail.com>
2025-08-31 00:26:12 -0300

    fsdfddfs

---
+++ src/routes/productosActualizados.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import {
+  listProducts,
+  getProductByBarcode,
+  getProductById,
+} from "../controllers/productosActualizados.js";
+
+const r = Router();
+
+r.get("/", listProducts);
+r.get("/by-barcode/:code", getProductByBarcode);
+r.get("/:id", getProductById);
+
+export default r;
